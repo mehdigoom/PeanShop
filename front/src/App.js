1ba319865ef119903c52608596483328288bd2e2
lamisy93 <ami.sy@hotmail.fr>
2019-07-11 10:36:30 +0200

    'update'

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './components/Header';
 import PeanutCard from './components/PeanutCard';
 import PeanutBasket from './components/PeanutBasket';
 import './App.scss';
@@ -47,9 +48,9 @@ class App extends React.Component {
             <PeanutBasket/>
             {/* <TestMiro /> */}
           </section>
-          <header>
-              <nav></nav>
-          </header>
+            <header>
+                <nav></nav>
+            </header>
           <section className="container -flex">
           <article className="delivery -flex">
                 <figure className="delivery-figure"><img src="https://i.imgur.com/KfBVf9w.jpg" alt="Delivery Image" /></figure>
---
+++ front/src/components/login/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import './styles.css'
+
+class Login extends Component {
+    render() {
+      return (
+        <form>
+          <label>
+            Login :
+            <input type="text" name="name" />
+            passeword :
+            <input type="password" name="Password" />
+          </label>
+          <input type="submit" value="Envoyer" />
+      </form>
+      )
+    }
+  }
+  
+  export default Login;
+  
